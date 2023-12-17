aa6dab8420fbd4129d2e620fd228f5b1575361a0
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-12-17 13:05:37 +0530

    Fetch data and add back button on Item Page

--- src/Components/Main/Item/Item.jsx
+++ src/Components/Main/Item/Item.jsx
@@ -1,14 +1,42 @@
-import { useParams } from "react-router-dom";
+import { useParams , useNavigate} from "react-router-dom";
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
+import { useEffect, useState } from "react";
+import Back from "./Assets/back.svg"
 
-export default function Item(){
+export default function Item() {
 
-    const {id ,name} = useParams()
+    const { id, name } = useParams()
+    const [loading, setLoading] = useState(true)
+    const [item, SetItem] = useState('')
+    let navigate = useNavigate()
+    useEffect(() => {
+        async function FetchItem() {
+            try {
+                const response = await fetch(`https://fakestoreapi.com/products/${id}`)
+                const json = await response.json();
+                SetItem(json)
+                setLoading(false)
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
+        FetchItem();
+    } , [])
     return (
         <>
-            <Header />
-            <Footer />
+            {loading
+                ?
+                <p>Loading...</p> 
+                :
+                <>
+                    <Header />
+                    <img src={Back} height="50px" width="50px" 
+                    style={{cursor: "pointer",position: "absolute"}} 
+                    onClick={() => navigate(-1)}></img>
+                    <Footer />
+                </>}
+
         </>
     )
 }
