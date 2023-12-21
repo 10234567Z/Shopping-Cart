035bcae1bbc1869f87db754f88a23d6f82bd420f
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-12-21 21:41:46 +0530

    Remove item from cart upon clicking remove button

--- src/Components/Main/Cart/Cart.jsx
+++ src/Components/Main/Cart/Cart.jsx
@@ -2,9 +2,11 @@ import { useLocation } from "react-router-dom";
 import Footer from "../../Footer/Footer";
 import Header from "../../Header/Header";
 import styles from "./cart.module.sass"
+import { useState } from "react";
 
 export default function Cart() {
     const location = useLocation();
+    const [reload , setReload] = useState(0)
     return (
         <>
             <Header count={location.state.cartCount} cart={location.state.cart} />
@@ -13,9 +15,12 @@ export default function Cart() {
                     <div className={`item ${el.id}`} key={`item ${el.id}`}>
                         <img src={el.image} width="200px" height="200px"></img>
                         <h1>{el.title}</h1>
-                        <h3>{el.price}</h3>
+                        <h3>${el.price}</h3>
                         <h4>Quantity: {el.quantity}</h4>
-                        <button>Remove</button>
+                        <button onClick={() => {
+                            location.state.cart.splice(location.state.cart.findIndex((it) => it.id === el.id) , 1)
+                            setReload(Math.random())
+                        }}>Remove</button>
                     </div>
                 ))}
             </main>
