8dbaa14af113729f79681d1b58b031c157383514
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-12-22 09:07:04 +0530

    Make a new better footer for website

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
-import './footer.sass'
+import styles from './footer.module.sass'
 export default function Footer(){
     return(
-        <footer>
-            <h5>Made by <a href="https://github.com/10234567Z">Fudo</a></h5>
+        <footer className={styles.footer}>
+            <div className={`gtk ${styles.div}`}>
+                <h2>Get To Know Us</h2>
+                <a href='#'>About Us</a>
+                <a href='#'>Careers</a>
+                <a href='#'>Press Releases</a>
+                <a href='#'>Web Science</a>
+            </div>
+            <div className={`connect ${styles.div}`}>
+                <h2>Connect With Us</h2>
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
+                <a href="#">Twitter</a>
+            </div>
         </footer>
     )
 }
