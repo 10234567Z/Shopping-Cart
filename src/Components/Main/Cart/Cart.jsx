0a24ae6e7b8602330f14f9297bd1cb7c6245d6ba
10234567Z <93607971+10234567Z@users.noreply.github.com>
2023-12-22 09:40:42 +0530

    Put message on cart page when 0 items in cart

--- src/Components/Main/Cart/Cart.jsx
+++ src/Components/Main/Cart/Cart.jsx
@@ -6,26 +6,28 @@ import { useState } from "react";
 
 export default function Cart() {
     const location = useLocation();
-    const [reload , setReload] = useState(0)
+    const [reload, setReload] = useState(0)
     return (
         <>
             <Header count={location.state.cartCount} cart={location.state.cart} />
             <main className={styles.main}>
-                {location.state.cart.map((el) => (
-                    <div className={`item ${el.id} ${styles.item}`} key={`item ${el.id}`}>
-                        <img src={el.image} width="200px" height="200px"></img>
-                        <div className={styles.tWrap}>
-                            <h1>{el.title}</h1>
-                        </div>
-                        <h3>${el.price}</h3>
-                        <h4>Quantity: {el.quantity}</h4>
-                        <button className={styles.btn} onClick={() => {
-                            location.state.cartCount -= el.quantity;
-                            location.state.cart.splice(location.state.cart.findIndex((it) => it.id === el.id) , 1)
-                            setReload(Math.random())
-                        }}>Remove</button>
-                    </div>
-                ))}
+                {location.state.cart.length >= 1
+                    ? location.state.cart.map((el) => (
+                        <div className={`item ${el.id} ${styles.item}`} key={`item ${el.id}`}>
+                            <img src={el.image} width="200px" height="200px"></img>
+                            <div className={styles.tWrap}>
+                                <h1>{el.title}</h1>
+                            </div>
+                            <h3>${el.price}</h3>
+                            <h4>Quantity: {el.quantity}</h4>
+                            <button className={styles.btn} onClick={() => {
+                                location.state.cartCount -= el.quantity;
+                                location.state.cart.splice(location.state.cart.findIndex((it) => it.id === el.id), 1)
+                                setReload(Math.random())
+                            }}>Remove</button>
+                        </div>))
+                    : <h1 className={styles.h1}>Nothing here....YET!</h1>
+                }
             </main>
             <Footer />
         </>
